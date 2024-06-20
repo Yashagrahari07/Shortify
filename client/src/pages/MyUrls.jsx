a9b3c7f1d843dacbe9b7dd3ddde3fb73dbe6a355
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-21 00:32:27 +0530

    Implemented MyUrls component with user URLs fetching

--- client/src/pages/MyUrls.jsx
+++ client/src/pages/MyUrls.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
 
 export default function MyUrls() {
+  const [urls, setUrls] = useState([]);
+  const currentUser = useSelector((state) => state.user.currentUser);
+
+  useEffect(() => {
+    const fetchUrls = async () => {
+      try {
+        const response = await fetch('/api/url/user/urls', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch URLs');
+        }
+
+        const data = await response.json();
+        setUrls(data);
+      } catch (error) {
+        console.error('Error fetching URLs:', error);
+      }
+    };
+
+    fetchUrls();
+  }, [currentUser]);
+
   return (
-    <div>
-      
+    <div className="max-w-6xl mx-auto py-10 px-4">
+      <h1 className="text-3xl font-bold text-center mb-6">My URL's</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {urls.map((url) => (
+          <div key={url.shortId} className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold mb-2 truncate">{url.originalUrl}</h2>
+            <p className="text-gray-600 mb-4 break-all">
+              Short URL: 
+              <Link to={`/${url.shortId}`} className="text-cyan-500 break-all">{`${window.location.origin}/api/url/${url.shortId}`}</Link>
+            </p>
+            <p className="text-gray-500">Expires at: {new Date(url.expiresAt).toLocaleDateString()}</p>
+            <p className="text-gray-500">Total Clicks: {url.visitHistory.length}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
