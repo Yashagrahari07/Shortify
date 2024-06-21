0f0d4e3059733528c85c7d9ba9bcd2a72d5cef98
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-22 00:31:03 +0530

    Configure redirects using netlify.toml

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,14 +13,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '.',
-        },
-      ],
-    }),
+    react()
   ],
 })
