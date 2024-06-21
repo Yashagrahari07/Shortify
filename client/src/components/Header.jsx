efbc52f7f769c14269108a4aa5b577462f61c315
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-21 15:17:01 +0530

    Update Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,17 +33,19 @@ export default function Header() {
                     Home
                   </Link>
                 </li>
-                <li className="my-5 md:my-0 md:mx-5">
-                  <Link
-                    to="/myurls"
-                    className={
-                      isActive("/myurls") ? "text-cyan-500" : "text-slate-400"
-                    }
-                  >
-                    My URL's
-                  </Link>
-                </li>
-                <li>
+                {currentUser && (
+                  <li className="my-5 md:my-0 md:ml-5">
+                    <Link
+                      to="/myurls"
+                      className={
+                        isActive("/myurls") ? "text-cyan-500" : "text-slate-400"
+                      }
+                    >
+                      My URLs
+                    </Link>
+                  </li>
+                )}
+                <li className="my-5 md:my-0 md:ml-5">
                   <Link
                     to="/about"
                     className={
@@ -57,8 +59,8 @@ export default function Header() {
             </nav>
           </article>
 
-          {currentUser ? ( // Conditionally rendering based on currentUser
-            <div className="hidden md:block">
+          <div className="hidden md:block">
+            {currentUser ? (
               <ul className="flex items-center ml-5">
                 <li className="my-5 md:my-0 md:mr-5">
                   <Link to="/profile" className="btn-cta rounded-full">
@@ -66,87 +68,97 @@ export default function Header() {
                   </Link>
                 </li>
               </ul>
-            </div>
-          ) : (
-            <>
-              <div className="hidden md:block">
-                <ul className="flex items-center ml-5">
-                  <li className="my-5 md:my-0 md:mr-5">
+            ) : (
+              <ul className="flex items-center ml-5">
+                <li className="my-5 md:my-0 md:mr-5">
+                  <Link to="/sign-in" className="text-slate-400">
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/sign-up" className="btn-cta rounded-full">
+                    Sign Up
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </div>
+
+          {isOpen && (
+            <div className="absolute left-5 right-5 top-20 rounded bg-slate-900 text-slate-200 text-center py-5 md:relative md:top-0 md:left-0 md:right-0 md:bg-transparent md:text-slate-700 md:text-left md:py-0 md:flex md:items-center">
+              <nav className="md:hidden">
+                <ul className="flex flex-col items-center justify-center">
+                  <li>
+                    <Link
+                      to="/"
+                      className={
+                        isActive("/") ? "text-cyan-500" : "text-slate-400"
+                      }
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  {currentUser && (
+                    <li className="mt-3">
+                      <Link
+                        to="/myurls"
+                        className={
+                          isActive("/myurls")
+                            ? "text-cyan-500"
+                            : "text-slate-400"
+                        }
+                      >
+                        My URLs
+                      </Link>
+                    </li>
+                  )}
+                  <li className="mt-3">
+                    <Link
+                      to="/about"
+                      className={
+                        isActive("/about")
+                          ? "text-cyan-500"
+                          : "text-slate-400"
+                      }
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <ul className="flex flex-col items-center justify-center">
+                {!currentUser && (
+                  <li className="mt-3">
                     <Link to="/sign-in" className="text-slate-400">
                       Login
                     </Link>
                   </li>
-                  <li>
+                )}
+                {!currentUser && (
+                  <li className="mt-5">
                     <Link to="/sign-up" className="btn-cta rounded-full">
                       Sign Up
                     </Link>
                   </li>
-                </ul>
-              </div>
-
-              {isOpen && (
-                <div className="absolute left-5 right-5 top-20 rounded bg-slate-900 text-slate-200 text-center py-10 md:relative md:top-0 md:left-0 md:right-0 md:bg-transparent md:text-slate-700 md:text-left md:py-0 md:flex md:items-center">
-                  <nav className="md:hidden">
-                    <ul className="flex flex-col items-center justify-center">
-                      <li>
-                        <Link
-                          to="/"
-                          className={
-                            isActive("/") ? "text-cyan-500" : "text-slate-400"
-                          }
-                        >
-                          Home
-                        </Link>
-                      </li>
-                      <li className="my-5">
-                        <Link
-                          to="/myurls"
-                          className={
-                            isActive("/myurls")
-                              ? "text-cyan-500"
-                              : "text-slate-400"
-                          }
-                        >
-                          My URL's
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/about"
-                          className={
-                            isActive("/about")
-                              ? "text-cyan-500"
-                              : "text-slate-400"
-                          }
-                        >
-                          About Us
-                        </Link>
-                      </li>
-                    </ul>
-                  </nav>
-                  <ul className="flex flex-col items-center justify-center">
-                    <li className="my-5">
-                      <Link to="/sign-in" className="text-slate-400">
-                        Login
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/sign-up" className="btn-cta rounded-full">
-                        Sign Up
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              )}
-
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="uppercase text-sm tracking-wide md:hidden"
-              >
-                {isOpen ? "Close" : "Menu"}
-              </button>
-            </>
+                )}
+                {currentUser && (
+                  <li className="mt-5">
+                    <Link to="/profile" className="btn-cta rounded-full">
+                      Profile
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
           )}
+
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="uppercase text-sm tracking-wide md:hidden"
+          >
+            {isOpen ? "Close" : "Menu"}
+          </button>
+
         </div>
       </header>
     </>
