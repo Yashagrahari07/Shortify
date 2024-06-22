523eca2c65e4efda9fc56510f6847f0552dd6db6
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-22 16:33:31 +0530

    Added favicon and dynamic page titles for each route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
 import MyUrls from "./pages/MyUrls.jsx";
@@ -10,35 +11,66 @@ import Redirect from "./pages/Redirect.jsx";
 import UrlDetails from "./pages/UrlDetails.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 
+function getPageTitle(pathname) {
+  switch (pathname) {
+    case '/':
+      return 'Shortify: Home';
+    case '/myurls':
+      return 'Shortify: My Urls';
+    case '/about':
+      return 'Shortify: About';
+    case '/sign-in':
+      return 'Shortify: Sign In';
+    case '/sign-up':
+      return 'Shortify: Sign Up';
+    case '/profile':
+      return 'Shortify: Profile';
+    default:
+      return 'Shortify';
+  }
+};
+
+function PageRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = getPageTitle(location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/:shortId/*" element={<Redirect />} />
+      
+      <Route element={<PrivateRoute/>}>
+        <Route path="/myurls" element={<MyUrls />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/details/:shortId/*" element={<UrlDetails />} />
+      </Route>
+      
+      <Route
+        path="/404"
+        element={
+          <div className="flex flex-col items-center">
+            <p className="text-lg text-black mb-4">404 Not Found !!</p>
+            <p className="text-sm text-gray-500">Oops! The page you are looking for does not exist.</p>
+          </div>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/:shortId/*" element={<Redirect />} />
-        
-        <Route element={<PrivateRoute/>}>
-          <Route path="/myurls" element={<MyUrls />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/details/:shortId/*" element={<UrlDetails />} />
-        </Route>
-        
-        <Route
-          path="/404"
-          element={
-            <div className="flex flex-col items-center">
-              <p className="text-lg text-black mb-4">404 Not Found !!</p>
-              <p className="text-sm text-gray-500">Oops! The page you are looking for does not exist.</p>
-            </div>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Routes>
+      <PageRoutes />
     </BrowserRouter>
   );
 }
